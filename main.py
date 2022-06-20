07de6ea0ece03208eb97b905a9f5c6ae67dacfb9
Gustavo Kazuo <kazucalado@gmail.com>
2022-06-20 17:57:20 -0300

    nada :(

--- main.py
+++ main.py
@@ -8,10 +8,4 @@ mydb = MySQLdb.connect(host='3306', user='root', passwd='2807', db='batata')
 with open('cars.csv') as csv_file:
     csv_file = csv.reader(csv_file, delimiter=',')
     for row in csv_file:
-        value = (row[0], row[1], row[2])
-        all_value.append(value)
-
-query = "INSERT INTO batata (Potato) VALUES (?)"
-mycursor = mydb.connect()
-mycursor.executemany(query, all_value)
-mydb.commit()
+        print(row)
